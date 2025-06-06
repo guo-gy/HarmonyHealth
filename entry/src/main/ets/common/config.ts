cbd192064d3ef704d7d24fbd8b10fcfdcdc1a0a0
yj <15292050844@qq.com>
2025-06-06 17:14:40 +0800

    Improve the interface

--- entry/src/main/ets/common/config.ts
+++ entry/src/main/ets/common/config.ts
@@ -4,6 +4,6 @@ export interface AppConfigType {
   USE_MOCK_DATA: boolean;
 }
 export const AppConfig = {
-  API_BASE_URL: 'http://192.168.184.254:8000',
+  API_BASE_URL: 'http://10.27.216.85:8000',
   USE_MOCK_DATA: false
 };
