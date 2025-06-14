db1aecca71469f7524456ca44eeccc2965d3a9c2
Cheng_Zezhong <981662187@qq.com>
2025-06-15 00:19:08 +0800

    天气

--- entry/src/main/ets/common/config.ts
+++ entry/src/main/ets/common/config.ts
@@ -4,6 +4,6 @@ export interface AppConfigType {
   USE_MOCK_DATA: boolean;
 }
 export const AppConfig = {
-  API_BASE_URL: 'http://10.27.243.89:8000',
+  API_BASE_URL: 'http://10.27.226.155:8000',
   USE_MOCK_DATA: false
 };
